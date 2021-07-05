4123b255d1490d5959e53288fc2d021be43eb36b
jeongseong-yong <puppy3987@gmail.com>
2021-07-05 22:08:21 +0900

    Initial commit

---
+++ chap03/chap03_01_08_BasicBuildingNodes.py
@@ -0,0 +1,15 @@
+import numpy as np
+class plus_node:
+    def __init__(self):
+        self._x = None
+        self._y = None
+        self._z = None
+
+    def forward(self, x, y):
+        self._x, self._y = x, y
+        self._z = self._x + self._y
+        return self._z
+
+    def backword(self, dz):
+        return dz, dz
+
