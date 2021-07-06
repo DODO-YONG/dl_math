6fef7ebc53e6a7f45753450a39c96a43c57b3d27
jeongseong-yong <puppy3987@gmail.com>
2021-07-06 14:03:15 +0900

    base_node

--- chap03/chap03_01_08_BasicBuildingNodes.py
+++ chap03/chap03_01_08_BasicBuildingNodes.py
@@ -13,3 +13,15 @@ class plus_node:
     def backword(self, dz):
         return dz, dz
 
+class minus_node:
+    def __init__(self):
+        self._x, self._y = None, None
+        self._z = None
+
+    def forward(self, x, y):
+        self._x, self._y = x, y
+        self._z = self._x - self._y
+        return self._z
+
+    def backward(self, dz):
+        return dz, -1*dz
